c2602a382ebecde06cfada2e8f4db8a2e2964b5e
wpsmith <t@wpsmith.net>
2019-02-20 20:56:50 -0500

    Update namespace/docs; general update of classes

--- src/Schemas.php
+++ src/Schemas.php
@@ -10,14 +10,14 @@
  *
  * @package    WPS\Schema
  * @author     Travis Smith <t@wpsmith.net>
- * @copyright  2015-2018 Travis Smith
+ * @copyright  2015-2019 Travis Smith
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
  * @link       https://github.com/wpsmith/WPS
  * @version    1.0.0
  * @since      0.1.0
  */
 
-namespace WPS\Schema;
+namespace WPS\WP\Schema;
 
 use WPS\Core\Singleton;
 
@@ -26,7 +26,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! class_exists( 'WPS\Schema\Schemas' ) ) {
+if ( ! class_exists( __NAMESPACE__ . '\Schemas' ) ) {
 	/**
 	 * Class Schemas.
 	 *
